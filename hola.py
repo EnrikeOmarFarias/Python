bfce4f74cbb7a27622db23a8d5c44eda930bc7fd
enriqueomarfarias <enrikefarias@gmail.com>
2024-08-06 16:56:23 -0300

    Aprendiendo Python

---
+++ hola.py
@@ -0,0 +1,8 @@
+print("hola mundo")
+
+def informacion(nombre, puesto = "Desconocido"):
+  print( f"soy {nombre} y soy {puesto}")
+
+informacion("Pedro", "programador")
+informacion("Juan", "catador de glandes")
+informacion("Lucas", "entrenador de canarios")
